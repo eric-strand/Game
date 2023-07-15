ddc809fedb3728468225bb18c270a313a39778b1
Anton <anton.smith.game@gmail.com>
2023-07-15 22:54:56 +0200

    Progress!

--- game.py
+++ game.py
@@ -21,31 +21,40 @@ while run:
 
     keys = pygame.key.get_pressed()
 
+
+    if x_pos <= 0:
+        x_pos = 0
+        x_direction = 0
+    elif x_pos >= 600:
+        x_pos = 600
+        x_direction = 0
+    if y_pos <= 0:
+        y_pos = 0
+        y_direction = 0
+    elif y_pos >= 600:
+        y_pos = 600
+        y_direction = 0
+
     if keys[pygame.K_UP]:
-        y_direction = -10
+        y_direction = -40
         x_direction = 0
     if keys[pygame.K_DOWN]:
-        y_direction = 10
+        y_direction = 40
         x_direction = 0
     if keys[pygame.K_LEFT]:
         y_direction = 0
-        x_direction = -10
+        x_direction = -40
     if keys[pygame.K_RIGHT]:
         y_direction = 0
-        x_direction = 10
+        x_direction = 40
     
     x_pos += x_direction
     y_pos += y_direction
     
     screen.fill(WHITE)
-    pygame.draw.rect(screen, GREEN, (x_pos, y_pos, 10, 10))
+    pygame.draw.rect(screen, GREEN, (x_pos, y_pos, 40, 40))
      
     pygame.display.update()
 
 pygame.quit()
 
-class Snake:
-    def __init__(self):
-        self.lenght = 1
-        self.body = 20
-        self.
