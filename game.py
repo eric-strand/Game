fde503ed9d8d02479970e5d16b0e13d3de28f38d
Anton <anton.smith.game@gmail.com>
2023-07-15 22:41:04 +0200

    Snake

--- game.py
+++ game.py
@@ -1,37 +1,51 @@
 import pygame
 
 pygame.init()
-
-screen = pygame.display.set_mode((1000,400))
-x = 500
+WIDTH = 640
+HEIGHT = 640
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+x_pos = y_pos = 320
+x_direction = 0
+y_direction = 0
 run = True
-# infinite loop 
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 while run:
-    # creates time delay of 10ms 
-    pygame.time.delay(10)
-      
-    # iterate over the list of Event objects  
-    # that was returned by pygame.event.get() method.  
-    for event in pygame.event.get():
-          
-        # if event object type is QUIT  
-        # then quitting the pygame  
-        # and program both.  
+    pygame.time.delay(100)  
+    for event in pygame.event.get():  
         if event.type == pygame.QUIT:
               
             # it will make exit the while loop 
             run = False
+
     keys = pygame.key.get_pressed()
 
+    if keys[pygame.K_UP]:
+        y_direction = -10
+        x_direction = 0
+    if keys[pygame.K_DOWN]:
+        y_direction = 10
+        x_direction = 0
     if keys[pygame.K_LEFT]:
-        x += -1
+        y_direction = 0
+        x_direction = -10
     if keys[pygame.K_RIGHT]:
-        x+= 1
-
-    screen.fill((255,255,255))
-
-    pygame.draw.circle(screen,(255,0,0), (x, 100), 20)
-
+        y_direction = 0
+        x_direction = 10
+    
+    x_pos += x_direction
+    y_pos += y_direction
+    
+    screen.fill(WHITE)
+    pygame.draw.rect(screen, GREEN, (x_pos, y_pos, 10, 10))
+     
     pygame.display.update()
 
 pygame.quit()
+
+class Snake:
+    def __init__(self):
+        self.lenght = 1
+        self.body = 20
+        self.
